d94a5f9bedcab85ab75c9844f0da95067c582d03
SamuelJenny <samuel@jenny.ms>
2023-04-16 10:37:50 +0200

    presale mint price

--- backend/src/config.js
+++ backend/src/config.js
@@ -82,7 +82,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0x4d4bB841016153642738c4ce7124b1279A5e10f6';
 const TREASURY_ADDRESS = '0x4d4bB841016153642738c4ce7124b1279A5e10f6';
 const MAX_SUPPLY = 10101; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.0001; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
